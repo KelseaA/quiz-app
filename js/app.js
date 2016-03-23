b8466f691356ae39a29904d0792106a89173f3b1
Kelsea <andersonkelsea@gmail.com>
2016-03-23 18:44:55 -0400

    Working on adding JSON

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
+$(document).ready(function(){
+	getQuestions();
+});
+
+// declaring global variables
+var questions = [];
+var nextQuestion = 0;
+
+// function to get json file
 function getQuestions(){
 	$.getJSON("questions.json", function(json){
-		questions = json.questions;
-		showQuestions();
-	});
+	questions = json.questions;
+	})
 }
+// gets next question of quiz
+function showNextQuestion(){
+	if(nextQuestion < questions.length){
+		showQuestions();
+	}
+}
+// gives feedback on answer choice - correct or incorrect
 function showAnswer(correct){
 	if(correct){
 		$(".pop-up").removeClass("pop-up-incorrect").addClass("pop-up-correct");
@@ -14,6 +29,7 @@ function showAnswer(correct){
 	$("body").addClass("pop-up-open");
 	$(".pop-up").fadeIn();
 }
+// shows questions on page
 function showQuestions(question, options, answerIndex){
 	$("#question-title").html(question);
 	$(".question-answers").empty();
@@ -25,6 +41,7 @@ function showQuestions(question, options, answerIndex){
 		$(".question-answers").append('<li data-correct-answer="'+correctAnswer+'">' + options[i] + '</li>');
 	}
 }
+// event listener to enable user to click on answer options
 $(".question-answers").on("click", ".answer-list", function(){
 	showAnswer($(this).data("correct-answer"));
 	console.log("You clicked this");
