49588ea2567f40f5e8e16a8fa9b8af3a4c48d3d6
Kelsea <andersonkelsea@gmail.com>
2016-03-26 12:14:19 -0400

    Working on js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
 $(document).ready(function(){
-	getQuestions();
+	startQuiz();
 });
 
 // declaring global variables
 var questions = [];
-var nextQuestion = 0;
+var nextQuestionIndex = 0;
+var score = 0;
+var questionCounter = 1;
 
 // function to get json file
 function getQuestions(){
 	$.getJSON("questions.json", function(json){
 	questions = json.questions;
+	startQuiz();
 	})
+}
+// starts new quiz
+function startQuiz(){
+	nextQuestionIndex = 0;
+	score = 0;
+	questionCounter = 1;
+	showNextQuestion();
 }
 // gets next question of quiz
 function showNextQuestion(){
-	if(nextQuestion < questions.length){
-		showQuestions();
+	if(nextQuestionIndex < questions.length){
+		showQuestions(questions[nextQuestionIndex].question, questions[nextQuestionIndex].options, questions[nextQuestionIndex].answerIndex);
+		nextQuestionIndex++;
+		questionCounter++;
+	}
+	else{
+		showUserScore();
 	}
 }
 // gives feedback on answer choice - correct or incorrect
@@ -40,6 +55,15 @@ function showQuestions(question, options, answerIndex){
 		}
 		$(".question-answers").append('<li data-correct-answer="'+correctAnswer+'">' + options[i] + '</li>');
 	}
+}
+// shows user's score after quiz is finished
+function showUserScore(){
+	// $(".question-title").empty();
+	// $(".question-answers").empty();
+	$("all-questions").hide();
+	$(".user-score").append(score);
+	$(".user-score").fadeIn();
+	$(".restart-quiz").fadeIn();
 }
 // event listener to enable user to click on answer options
 $(".question-answers").on("click", ".answer-list", function(){
